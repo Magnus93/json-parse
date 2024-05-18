cf4a3d11248773d91f73bd3d8049ef2732470722
Magnus93 <magnus.ja.gustafsson@gmail.com>
2024-05-18 21:55:15 +0200

    Position Zero

--- Position.ts
+++ Position.ts
@@ -9,7 +9,9 @@ export namespace Position {
 	export const type = isly.object<Position>({ row: isly.number(), column: isly.number() }, "Position")
 	export const is = type.is
 	export const flaw = type.flaw
-	export const zero: Position = { row: 0, column: 0 } as const
+	export function zero(): Position {
+		return { row: 0, column: 0 } as const
+	}
 	export function add(left: Position, right: string): Position {
 		const splitted = right.split("\n")
 		return splitted.length > 1
